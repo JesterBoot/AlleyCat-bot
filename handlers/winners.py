9a78bf62a9702bf66d99b81b3c0fab5f9f2ba59c
StGrail <zzmrlol@gmail.com>
2020-08-27 21:04:27 +0300

    Ура, разобрался с виннерами. Нужно расписать каждую категорию

--- handlers/winners.py
+++ handlers/winners.py
@@ -1,23 +1,16 @@
-import re
-
-from aiogram.dispatcher.filters.builtin import Command
-from data.config import admins
 from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters.builtin import Command
 
+from data.config import admins
 from loader import dp, db
 
-'''н'''
+'''Победители фиксы, мужской зачет'''
 @dp.message_handler(Command('winner_fixie_men'))
-async def get_winners(message: types.Message):
-    male_winners = str(await db.male_fixie_winners())
-    print(male_winners)
-    male_winners = male_winners.replace("<Record name='", '')
-    male_winners = male_winners.replace(""time='")
-    male_winners = male_winners.split('_')
-    print(male_winners)
-
-    # await message.answer(winners)
-    # for admin in admins:
-    # await message.reply(f'Победители фиксы, мужской зачет: {male_winners}')
-
-    #     print() По команде/winner список всех победителей во всех зачетах
+async def get_winners(dp: Dispatcher):
+    result = ''
+    male_winners = dict(await db.male_fixie_winners())
+    for key, value in male_winners.items():
+        result += key + ' - '
+        result += value + '\n'
+    for admin in admins:
+        await dp.bot.send_message(admin, text=f'Победители фиксы, мужской зачет:\n{result}')
