5ea292cc7f151a90a5021ed2f78a0086ae7b5380
StGrail <zzmrlol@gmail.com>
2020-08-29 22:56:50 +0300

    Гитигнор, .env, текст в отдельный файл, константы

--- data/config.py
+++ data/config.py
@@ -4,17 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#id: Мой, Олега
-admins = [412112889,
-          139148302,]
-
-
-
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 PGUSER = str(os.getenv("PGUSER"))
 PGPASSWORD = str(os.getenv("PGPASSWORD"))
 DATABASE = str(os.getenv("DATABASE"))
 ip = os.getenv("ip")
+
+# id: Мой, Олега
+admins = [412112889,
+          139148302,
+          ]
 
 aiogram_redis = {
     'host': ip,
