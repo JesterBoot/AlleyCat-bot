2d58e1b9cf1f41cd9303ec70266b858bcb834ac4
g <zzmrlol@gmail.com>
2021-03-11 20:32:49 +0300

    Change const. DATABASE to DATABASE_NAME

--- utils/config.py
+++ utils/config.py
@@ -5,10 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+DJANGO_SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
+
+DATABASE = str(os.getenv('DATABASE_NAME'))
 PGUSER = str(os.getenv('PGUSER'))
 PGPASSWORD = str(os.getenv('PGPASSWORD'))
-DATABASE = str(os.getenv('DATABASE'))
 ip = os.getenv('ip')
+
 admins = int(os.getenv('ADMINS'))
 
 aiogram_redis = {
