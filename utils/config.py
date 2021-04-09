ca44f5c6579a01c9877ab69ed61f478d5bec3e44
G <zzmrlol@gmail.com>
2021-04-09 21:17:45 +0300

    Delete ip

--- utils/config.py
+++ utils/config.py
@@ -8,16 +8,16 @@ BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 PGUSER = str(os.getenv('PGUSER'))
 PGPASSWORD = str(os.getenv('PGPASSWORD'))
 DATABASE = str(os.getenv('DATABASE_NAME'))
-ip = os.getenv('ip')
+# ip = os.getenv('ip')
 admins = int(os.getenv('ADMINS'))
 
-aiogram_redis = {
-    'host': ip,
-}
-
-redis = {
-    'address': (ip, 6379),
-    'encoding': 'utf8'
-}
+# aiogram_redis = {
+#     'host': ip,
+# }
+#
+# redis = {
+#     'address': (ip, 6379),
+#     'encoding': 'utf8'
+# }
 
 # POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}"
