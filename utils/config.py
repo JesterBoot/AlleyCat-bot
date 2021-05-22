d2820eee80dead0babb2c07cdf811b239a31ef97
StGrail <zzmrlol@gmail.com>
2021-05-22 12:02:47 +0300

    Add admins id

--- utils/config.py
+++ utils/config.py
@@ -10,18 +10,8 @@ DB_PASSWORD = str(os.getenv('PGPASSWORD'))
 DB_NAME = str(os.getenv('DATABASE_NAME'))
 DB_HOST = str(os.getenv('DATABASE_HOST'))
 
-
-
-# ip = os.getenv('ip')
-admins = int(os.getenv('ADMINS'))
-
-# aiogram_redis = {
-#     'host': ip,
-# }
-#
-# redis = {
-#     'address': (ip, 6379),
-#     'encoding': 'utf8'
-# }
-
-# POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{ip}/{DATABASE}"
+admins = [
+    412112889,
+    # 139148302,  # Oleg
+    # 108554,  # Nikita
+]
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -3,10 +3,13 @@ import os
 
 from aiogram import Dispatcher
 
+from utils.config import admins
+
 
 async def on_startup_notify(dp: Dispatcher):
     try:
-        await dp.bot.send_message(chat_id=os.getenv('ADMINS'), text='Бот Запущен')
+        for admin in admins:
+            await dp.bot.send_message(chat_id=admin, text='Бот Запущен')
 
     except Exception as err:
         logging.exception(err)
