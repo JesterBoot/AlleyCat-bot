5f52c28d7cab258c65f3d412503d67362f792014
StGrail <zzmrlol@gmail.com>
2021-05-22 12:39:36 +0300

    Add admins id

--- utils/config.py
+++ utils/config.py
@@ -11,7 +11,7 @@ DB_NAME = str(os.getenv('DATABASE_NAME'))
 DB_HOST = str(os.getenv('DATABASE_HOST'))
 
 admins = [
-    412112889,
+    412112889,  # My
     # 139148302,  # Oleg
     # 108554,  # Nikita
 ]
