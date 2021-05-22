27752810fdde6538547dbb61fd95e5f84c81cfa1
StGrail <zzmrlol@gmail.com>
2021-05-22 13:06:57 +0300

    Change bot command

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,5 +4,5 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand('help', 'Жми, если не можешь отправить локацию'),
-        types.BotCommand('results', 'Посмотреть общий зачет гонки')
+        types.BotCommand('racers', 'Посмотреть всех участников гонки')
     ])
