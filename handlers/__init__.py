284c758312422eee3531909994447acfaa1c2b48
StGrail <zzmrlol@gmail.com>
2021-05-28 01:52:47 +0300

    Add commands for send messages for all users * update admin messages

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,7 +4,9 @@ from .registration import dp
 from .race import dp
 from .delete import dp
 from .winners import dp
+from .command_info import dp
 from .media_catch import dp
+
 
 
 __all__ = ["dp"]
---
+++ handlers/command_info.py
@@ -0,0 +1,45 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import Command
+
+from FSM.Race_states import Race
+from constants.text_messages import RACE_MECHANIC
+from keyboards.inline_kb import are_you_ready
+from utils.config import admins
+from utils.loader import dp, db
+
+
+@dp.message_handler(Command('send_all'), user_id=admins)
+async def send_all(message: types.Message):
+    racers = await db.select_all_racers()
+    mess = message.text[9:]
+    if len(mess) < 1:
+        await message.answer("Слишком короткое сообщение.")
+    else:
+        if len(racers) > 0:
+            for racer in racers:
+                await dp.bot.send_message(racer['id'], mess)
+        else:
+            await message.answer("Пока никто не зарегистрировался.")
+
+
+@dp.message_handler(Command('send'), user_id=admins)
+async def send_mechanic(message: types.Message):
+    racers = await db.select_all_racers()
+    if len(racers) > 0:
+        for racer in racers:
+            await dp.bot.send_message(racer['id'], text=RACE_MECHANIC)
+    else:
+        await message.answer("Пока никто не зарегистрировался.")
+
+
+@dp.message_handler(Command('fail'), user_id=admins)
+async def fail(message: types.Message):
+    """ Жми команду, если ничего не сработает автоматически. """
+
+    racers = await db.select_all_racers()
+    if len(racers) > 0:
+        for racer in racers:
+            await dp.bot.send_message(racer['id'], 'Ты готов к гонке?', reply_markup=are_you_ready)
+            await Race.FIRST_POINT.set()
+    else:
+        await message.answer("Пока никто не зарегистрировался.")
