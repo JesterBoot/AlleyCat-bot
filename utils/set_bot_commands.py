ca238c287c16d0338f8f7088129b193dbb1496bf
StGrail <zzmrlol@gmail.com>
2021-06-05 13:02:00 +0300

    Add new command /info

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,5 +4,6 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand('help', 'Жми, если не можешь отправить локацию'),
-        types.BotCommand('racers', 'Посмотреть всех участников гонки')
+        types.BotCommand('racers', 'Посмотреть всех участников гонки'),
+        types.BotCommand('info', 'Дополнительная информация о гонке'),
     ])
